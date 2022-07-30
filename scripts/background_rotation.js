2347b507bca4af4d04854b62a3f4dac8dff9087a
Jacksaur <58850292+Jacksaur@users.noreply.github.com>
2022-07-30 09:18:43 +0100

    Contributed by u/13z_-

---
+++ scripts/background_rotation.js
@@ -0,0 +1,17 @@
+// Load the root css variables
+style_root = document.querySelector(':root')
+
+// Set the images to be used
+images = [
+    'image2.png', 'image3.png', 'image3.png' 
+]
+
+function random_image(){
+    // Get a random index postion of the images array
+    array_index = Math.floor(Math.random() * images.length)
+
+    // Change the --img variable based on the random index
+    style_root.style.setProperty('--background_image', `url(images/pictures/${images[array_index]})`)
+}
+
+random_image()
